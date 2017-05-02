ac346f7255ea52853fd1c9eda21ac22dee53d95a
niyas <niyasahamedpkd@gmil.com>
2017-05-02 22:10:23 +0530

    postgres addition

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -64,11 +64,14 @@ WSGI_APPLICATION = 'socialmedia.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'socialmedia',
+        'USER': 'niyas',
+        'PASSWORD': 'niyas1739',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
