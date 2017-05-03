34600b8123151b4b384e0ff991e101e0dd9c449e
niyas <niyasahamedpkd@gmil.com>
2017-05-03 11:42:48 +0530

    removed dj

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import dj_database_url
+
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -27,7 +27,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = [ 'https://django-social.herokuapp.com/' ]
+ALLOWED_HOSTS = [ 'https://django-social.herokuapp.com/', * ]
 
 
 # Application defin
@@ -83,7 +83,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] =  dj_database_url.config()
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
